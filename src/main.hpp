5946112018412119a443d621f16dd34fa46087e0
natokra <nmk_10@hotmail.com>
2026-04-28 14:36:53 -0300

    Update main.hpp

--- src/main.hpp
+++ src/main.hpp
@@ -671,9 +671,9 @@ typedef struct door_t
 #define TEXTURESIZE 32
 #define TEXTUREPOWER 5 // power of 2 that texture size is, ie pow(2,TEXTUREPOWER) = TEXTURESIZE
 #ifdef BARONY_SUPER_MULTIPLAYER
-#define MAXPLAYERS 8
+#define MAXPLAYERS 15
 #else
-#define MAXPLAYERS 4
+#define MAXPLAYERS 15
 #endif
 
 // shaking/bobbing, that sort of thing
